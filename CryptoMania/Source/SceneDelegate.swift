c1875080bb642ecb264a46784ad70855123c7d6a
John Motta <gabriel_motta31@hotmail.com>
2024-10-22 09:08:36 -0300

    Create nav

--- CryptoMania/Source/SceneDelegate.swift
+++ CryptoMania/Source/SceneDelegate.swift
@@ -14,10 +14,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
+        let window = UIWindow(windowScene: windowScene)
         let vc = LoginViewController()
-        window?.rootViewController = UINavigationController(rootViewController: vc)
-        window?.makeKeyAndVisible()
+        let nav =  UINavigationController(rootViewController: vc)
+        window.rootViewController = nav
+        self.window = window
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
